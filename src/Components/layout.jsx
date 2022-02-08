121b5ce9811752a3389f253e9848d27bf2d1c33a
wildething96 <85587609+wildething96@users.noreply.github.com>
2022-02-08 11:40:41 +0000

    Change to useEffect

--- src/Components/layout.jsx
+++ src/Components/layout.jsx
@@ -55,11 +55,13 @@ const Layout = () => {
   }, [list]);
 
   useEffect(() => {
+    async () => {
       const response = await fetch(
         "https://api.thecatapi.com/v1/images/search?limit=10"
       );
       const data = await response.json();
       handleData(data);
+    };
   }, []);
 
 
