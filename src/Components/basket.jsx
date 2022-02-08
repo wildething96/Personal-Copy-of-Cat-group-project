df7b78efc6cece2a9b7feb51c6fc9ee915ab14fc
wildething96 <85587609+wildething96@users.noreply.github.com>
2022-02-08 11:05:50 +0000

    removed unused imports

--- src/Components/basket.jsx
+++ src/Components/basket.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import styled, { css } from "styled-components";
+import React, { useState} from "react";
+import styled from "styled-components";
 // import { motion } from "framer-motion";
 
 
