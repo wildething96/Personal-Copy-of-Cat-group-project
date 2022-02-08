dc59c149110499e581a02571a5462edd20a16ed8
wildething96 <85587609+wildething96@users.noreply.github.com>
2022-02-08 11:42:58 +0000

    Change to useEffect

--- src/Components/layout.jsx
+++ src/Components/layout.jsx
@@ -54,14 +54,12 @@ const Layout = () => {
       setTotal([currentTotal.toFixed(2), currentQuantity]);
   }, [list]);
 
-  useEffect(() => {
-    async () => {
+  useEffect(async() => {
       const response = await fetch(
         "https://api.thecatapi.com/v1/images/search?limit=10"
       );
       const data = await response.json();
       handleData(data);
-    };
   }, []);
 
 
